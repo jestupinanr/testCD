b784c4941e54c6182759417b891d6673175725e4
juanestupinan <juan.estupinan@antpack.co>
2022-11-15 19:47:03 -0500

    :hammer: new title

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const PokemonList = ({ pokemonList }) => {
   return (
     <>
-      <h1 className='list__title'>PokeList</h1>
+      <h1 className='list__title'>modificado Norbey Danilo Muñoz Cañon (corazon) </h1>
       <div className="list-container">
         {pokemonList.map(({ id, name }) => (
           <Link key={id} to={`/pokemon/${name}`} className="list-item" style={{ backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`})` }}>
